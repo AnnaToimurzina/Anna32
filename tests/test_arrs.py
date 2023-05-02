67545497d5c961da604eee44b03b21711667c8fe
AnnaToimurzina <2402as@gmail,com>
2023-05-03 01:04:09 +0500

    тесты работают100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,7 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, 4) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], -3) == [2, 3, 4]
+    assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
